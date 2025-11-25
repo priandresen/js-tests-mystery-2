681b85f8385f83b71de0dc99398a01c602215209
Priscilla Andresen <pgdamorim@gmail.com>
2025-11-25 12:18:23 -0800

    implements mystery2

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,9 @@
-export const mystery2 = (num) => {};
+export const mystery2 = (num) => {
+  if (num % 4 !== 0) {
+    return false;
+  } else if (num % 100 === 0 && num % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
